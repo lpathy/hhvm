b03bf68fd7554c15dc567773a0ea8f28808c180a
lpathy <lpathy@cavium.com>
2016-04-05 14:20:35 -0700

    Fixed decode issues in arm64 cases
    1. Added arm64 jcc decoding in mc-generator.cpp
    2. Added arm64 framePtr save in target-cache.cpp
    3. Fixed stub_size and extract_spoff for arm64 in service-requests.cpp
    4. Fixed emitPredictionsAndPreConditions for arm64 in translate-region.cpp

--- hphp/runtime/vm/jit/service-requests.cpp
+++ hphp/runtime/vm/jit/service-requests.cpp
@@ -222,6 +222,12 @@ namespace x64 {
   static constexpr int kLeaVmSpLen = 7;
 }
 
+namespace arm {
+  // 'lea' results in atmost 4 isntructions (see vasm-arm.cpp)
+  static constexpr int kMovLen = 4 * 4;
+  static constexpr int kLeaVmSpLen = 4 * 4;
+}
+
 size_t stub_size() {
   // The extra args are the request type and the stub address.
   constexpr auto kTotalArgs = kMaxArgs + 2;
@@ -230,7 +236,7 @@ size_t stub_size() {
     case Arch::X64:
       return kTotalArgs * x64::kMovLen + x64::kLeaVmSpLen;
     case Arch::ARM:
-      not_implemented();
+      return kTotalArgs * arm::kMovLen + arm::kLeaVmSpLen;
     case Arch::PPC64:
       not_implemented();
   }
@@ -254,7 +260,25 @@ FPInvOffset extract_spoff(TCA stub) {
       }
 
     case Arch::ARM:
-      not_implemented();
+      {
+        struct Decoder : public vixl::Decoder {
+          void VisitAddSubImmediate(vixl::Instruction* inst) {
+            // For immediate operands, shift can be '0' or '12'
+            int64_t immed =
+              inst->ImmAddSub() << ((inst->ShiftAddSub() == 1) ? 12 : 0);
+            switch (inst->Mask(vixl::AddSubOpMask)) {
+              case vixl::ADD: offset = immed; break;
+              case vixl::SUB: offset = -immed; break;
+              default: break;
+            }
+          }
+          folly::Optional<int32_t> offset;
+        };
+        Decoder decoder;
+        decoder.Decode((vixl::Instruction*)(stub));
+        always_assert(decoder.offset && (*decoder.offset % sizeof(Cell)) == 0);
+        return FPInvOffset{-(*decoder.offset / int32_t{sizeof(Cell)})};
+      }
 
     case Arch::PPC64:
       not_implemented();
